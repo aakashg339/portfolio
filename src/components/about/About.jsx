2e9c22191df0c264c5a2d41e5484477d23af766e
aakashg339 <141503528+aakashg339@users.noreply.github.com>
2026-05-08 08:54:24 +0530

    feat: Added About me section

---
+++ src/components/about/About.jsx
@@ -0,0 +1,26 @@
+function About() {
+    return (
+        <>
+            <section id="" className="px-6 py-16 bg-white transition-colors">
+                <div className="max-w-6xl mx-auto">
+                    <h2 className="text-3xl mb-8 text-slate-900 border-l-4 border-blue-600 pl-4">
+                        About Me
+                    </h2>
+                    <div className="space-y-4 text-lg text-slate-700 leading-relaxed max-w-3xl">
+                        <p className="">
+                            More details about myself
+                        </p>
+                        <p className="">
+                            More details about myself
+                        </p>
+                        <p className="">
+                            More details about myself
+                        </p>
+                    </div>
+                </div>
+            </section>
+        </>
+    );
+}
+
+export default About;
