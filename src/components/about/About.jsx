27969238072d3833d0503cc2cc467894ab93ceba
aakashg339 <141503528+aakashg339@users.noreply.github.com>
2026-05-08 08:58:46 +0530

    docs: Added comment in About component

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -3,9 +3,12 @@ function About() {
         <>
             <section id="" className="px-6 py-16 bg-white transition-colors">
                 <div className="max-w-6xl mx-auto">
+                    {/* Section Heading */}
                     <h2 className="text-3xl mb-8 text-slate-900 border-l-4 border-blue-600 pl-4">
                         About Me
                     </h2>
+
+                    {/* Details */}
                     <div className="space-y-4 text-lg text-slate-700 leading-relaxed max-w-3xl">
                         <p className="">
                             More details about myself
