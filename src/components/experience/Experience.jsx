beb78b2ce80e028ff4a8e1d22daf8816514b8eda
aakashg339 <141503528+aakashg339@users.noreply.github.com>
2026-05-10 11:53:18 +0530

    refactor: experience section getting data from config file

--- src/components/experience/Experience.jsx
+++ src/components/experience/Experience.jsx
@@ -1,25 +1,11 @@
+import portfolioConfig from "../../config/portfolioConfig";
 import SectionHeading from "../common/SectionHeading";
 import ExperienceItem from "./ExperienceItem";
 
 function Experience() {
-    const experiences = [
-        {
-            id: 1,
-            designation: "Some Designaion", 
-            companyName: "Some company", 
-            yearFrom: "2023", 
-            yearTo: "2024", 
-            description: "Did something for the company"
-        },
-        {
-            id: 2,
-            designation: "Some Designaion 2", 
-            companyName: "Some company 2", 
-            yearFrom: "2023", 
-            yearTo: "2024", 
-            description: "Did something for the company 2"
-        }
-    ];
+    const experienceSectionData = portfolioConfig.experience;
+    
+    const experiences = experienceSectionData.experiences;
 
     return (
         <>
--- src/config/portfolioConfig.js
+++ src/config/portfolioConfig.js
@@ -10,6 +10,26 @@ const portfolioConfig = {
             "More details about myself",
             "More details about myself"
         ]
+    },
+    experience: {
+        experiences: [
+            {
+                id: 1,
+                designation: "Some Designaion", 
+                companyName: "Some company", 
+                yearFrom: "2023", 
+                yearTo: "2024", 
+                description: "Did something for the company"
+            },
+            {
+                id: 2,
+                designation: "Some Designaion 2", 
+                companyName: "Some company 2", 
+                yearFrom: "2023", 
+                yearTo: "2024", 
+                description: "Did something for the company 2"
+            }
+        ]
     }
 }
 
